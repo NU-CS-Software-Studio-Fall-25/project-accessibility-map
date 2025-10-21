fc772850e3083578fcd4475c5210f66884eb0c75
ysilksa <isabella.yan.business@gmail.com>
2025-10-21 00:37:33 -0500

    feat: location_features join table

---
+++ db/migrate/20251021052225_create_join_table_locations_features.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableLocationsFeatures < ActiveRecord::Migration[8.0]
+  def change
+    create_join_table :locations, :features, table_name: :locations_features do |t|
+      t.index [:location_id, :feature_id]
+      t.index [:feature_id, :location_id]
+    end
+  end
+end
