3470c4fc45f9efe4bfee70d03288b41056d0d4d3
brndn-do <dobrandon05@gmail.com>
2025-10-24 17:49:14 -0500

    chore(auth): Update DB Schema
    - Ran rails generate authentication
    - Added user field to locations and reviews with migration

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,31 +10,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_11_184153) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_24_224749) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
 
   create_table "locations", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.text("name")
-    t.text("address")
-    t.text("city")
-    t.text("state")
-    t.text("zip")
-    t.text("country")
-    t.decimal("latitude", precision: 10, scale: 8)
-    t.decimal("longitude", precision: 11, scale: 8)
-    t.datetime("created_at", null: false)
-    t.datetime("updated_at", null: false)
+    t.text "name"
+    t.text "address"
+    t.text "city"
+    t.text "state"
+    t.text "zip"
+    t.text "country"
+    t.decimal "latitude", precision: 10, scale: 8
+    t.decimal "longitude", precision: 11, scale: 8
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.uuid "user_id", null: false
+    t.index ["user_id"], name: "index_locations_on_user_id"
   end
 
   create_table "reviews", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.text("body")
-    t.datetime("created_at", null: false)
-    t.datetime("updated_at", null: false)
-    t.uuid("location_id")
-    t.index(["location_id"], name: "index_reviews_on_location_id")
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.uuid "location_id"
+    t.uuid "user_id", null: false
+    t.index ["location_id"], name: "index_reviews_on_location_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
+  create_table "sessions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  add_foreign_key "locations", "users"
   add_foreign_key "reviews", "locations", on_delete: :cascade
+  add_foreign_key "reviews", "users"
+  add_foreign_key "sessions", "users"
 end
