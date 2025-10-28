8a6b33e92739e6851e7dfe442b240d7ab19687ce
Larry Ling <larryling.main@gmail.com>
2025-10-28 12:57:55 -0500

    even more fixes to solid cache table

---
+++ db/migrate/20251028175703_fix_solid_cache_entries_byte_size.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class FixSolidCacheEntriesByteSize < ActiveRecord::Migration[7.2]
+  def change
+    # Change byte_size to bigint to avoid integer range errors
+    change_column :solid_cache_entries, :byte_size, :bigint, null: false
+  end
+end
