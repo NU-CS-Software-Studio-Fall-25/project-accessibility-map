4b7b494a6f7c4bdf0f605d1d2e380b2b27736db7
brndn-do <dobrandon05@gmail.com>
2025-11-05 17:30:10 -0600

    feat(auth): Add redirect to login page after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to(root_path, notice: "Account created successfully!")
+      redirect_to(new_session_path, notice: "Account created successfully! Please sign in.")
     else
       render(:new, status: :unprocessable_entity)
     end
