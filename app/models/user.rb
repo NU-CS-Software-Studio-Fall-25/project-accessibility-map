e1bf23d2d51bc574ec59bb842b903181c66a4b2a
brndn-do <dobrandon05@gmail.com>
2025-11-05 16:58:06 -0600

    fix(auth): Validate uniqueness of email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 class User < ApplicationRecord
   has_secure_password
+  validates :email_address, uniqueness: true
   has_many :sessions, dependent: :destroy
   has_many :locations
   has_many :reviews
