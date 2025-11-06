b8a2b3dcce95919abcf1617d76e27df9f3a8cd7f
brndn-do <dobrandon05@gmail.com>
2025-11-06 00:07:01 -0600

    Chore: standardize on using npm, remove pnpm-lock.yaml

--- db/schema.rb
+++ db/schema.rb
@@ -232,9 +232,4 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_28_180254) do
   add_foreign_key "reviews", "locations", on_delete: :cascade
   add_foreign_key "reviews", "users"
   add_foreign_key "sessions", "users"
-  add_foreign_key "solid_queue_blocked_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
-  add_foreign_key "solid_queue_claimed_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
-  add_foreign_key "solid_queue_failed_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
-  add_foreign_key "solid_queue_ready_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
-  add_foreign_key "solid_queue_recurring_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
 end
