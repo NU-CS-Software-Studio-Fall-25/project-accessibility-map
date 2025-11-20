c772a90427a850da6c0fe161accacaa06b2de111
brndn-do <dobrandon05@gmail.com>
2025-11-19 20:44:29 -0600

    feat: Implement sign-in with Google
    - Installed necessary omniauth gems
    - Added OmniAuth initializer to configure Google provider and load API keys from environment variables
    - Added a route to handle /auth/google_oauth2/callback
    - Migrated the `users` table to include 2 columns: `provider` (Google) and `uid` (from Google, not our primary key)
    - Updated sessions controller to support both types of login
    - Added a sign in with google button to both the login page and signup page
    - Deleted session_store.rb because it was disabling session store

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,7 @@
+OmniAuth.config.allowed_request_methods = [:post, :get]
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2,
+    ENV["GOOGLE_OAUTH_CLIENT_ID"],
+    ENV["GOOGLE_OAUTH_CLIENT_SECRET"]
+end 
