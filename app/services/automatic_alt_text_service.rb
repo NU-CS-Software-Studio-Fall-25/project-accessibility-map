c83b015fc6199445db375a3e768c77c0e5018995
ysilksa <isabella.yan.business@gmail.com>
2025-12-05 16:49:41 -0600

    fix: fix API key var name and JSON parsing

--- app/services/automatic_alt_text_service.rb
+++ app/services/automatic_alt_text_service.rb
@@ -5,7 +5,7 @@ class AutomaticAltTextService
   HF_MODEL = "Salesforce/blip-image-captioning-base"
 
   def self.generate_for(attachment)
-    return unless attachment.variable? 
+    return unless attachment.variable?
 
     image_url = Rails.application.routes.url_helpers.rails_blob_url(
       attachment,
@@ -23,7 +23,7 @@ class AutomaticAltTextService
   end
 
   def self.call_huggingface(image_url)
-    api_key = Rails.application.credentials.dig(:huggingface, :api_key)
+    api_key = ENV["HUGGINGFACE_API_KEY"] 
     uri = URI("https://api-inference.huggingface.co/models/#{HF_MODEL}")
 
     headers = {
@@ -36,6 +36,6 @@ class AutomaticAltTextService
     response = Net::HTTP.post(uri, payload, headers)
     json = JSON.parse(response.body)
 
-    json.first["generated_text"] rescue nil
+    json[0]["generated_text"] rescue nil
   end
 end
